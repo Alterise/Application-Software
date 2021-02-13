180e8699f703f8d93978aba01202a557d6c36521
Alex <alex.tereshkov01@mail.ru>
2021-02-14 01:51:02 +0300

    Add Lab1

---
+++ 2nd semester/Lab1/Lab1/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace Lab1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.dataGridView3 = new System.Windows.Forms.DataGridView();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(14, 14);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(774, 574);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.label5);
+			this.tabPage1.Controls.Add(this.button1);
+			this.tabPage1.Controls.Add(this.label4);
+			this.tabPage1.Controls.Add(this.label3);
+			this.tabPage1.Controls.Add(this.label2);
+			this.tabPage1.Controls.Add(this.label1);
+			this.tabPage1.Controls.Add(this.textBox4);
+			this.tabPage1.Controls.Add(this.textBox3);
+			this.tabPage1.Controls.Add(this.textBox2);
+			this.tabPage1.Controls.Add(this.textBox1);
+			this.tabPage1.Controls.Add(this.dataGridView1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 24);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(766, 546);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Task 1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(115, 376);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(145, 41);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "Вычислить";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(114, 284);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(26, 19);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "N:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(108, 209);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(31, 19);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "dx:";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(108, 141);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(31, 19);
+			this.label2.TabIndex = 6;
+			this.label2.Text = "x1:";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(114, 77);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(25, 19);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "A:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(145, 284);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 23);
+			this.textBox4.TabIndex = 4;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(145, 209);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 23);
+			this.textBox3.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(145, 141);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 23);
+			this.textBox2.TabIndex = 2;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(145, 77);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 23);
+			this.textBox1.TabIndex = 1;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(389, 24);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersWidth = 4;
+			this.dataGridView1.Size = new System.Drawing.Size(331, 489);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.dataGridView3);
+			this.tabPage2.Controls.Add(this.button2);
+			this.tabPage2.Controls.Add(this.label6);
+			this.tabPage2.Controls.Add(this.dataGridView2);
+			this.tabPage2.Location = new System.Drawing.Point(4, 24);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(766, 546);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Task 2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.AllowUserToAddRows = false;
+			this.dataGridView2.AllowUserToDeleteRows = false;
+			this.dataGridView2.AllowUserToResizeColumns = false;
+			this.dataGridView2.AllowUserToResizeRows = false;
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(173, 103);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.RowHeadersWidth = 4;
+			this.dataGridView2.Size = new System.Drawing.Size(408, 109);
+			this.dataGridView2.TabIndex = 0;
+			this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(140, 24);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(111, 29);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Введите:";
+			this.label5.Click += new System.EventHandler(this.label5_Click_1);
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(257, 35);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(240, 29);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Заполните матрицу:";
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(303, 284);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(155, 69);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "Вычислить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// dataGridView3
+			// 
+			this.dataGridView3.AllowUserToAddRows = false;
+			this.dataGridView3.AllowUserToDeleteRows = false;
+			this.dataGridView3.AllowUserToResizeColumns = false;
+			this.dataGridView3.AllowUserToResizeRows = false;
+			this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView3.Location = new System.Drawing.Point(173, 404);
+			this.dataGridView3.Name = "dataGridView3";
+			this.dataGridView3.ReadOnly = true;
+			this.dataGridView3.RowHeadersWidth = 4;
+			this.dataGridView3.Size = new System.Drawing.Size(408, 37);
+			this.dataGridView3.TabIndex = 13;
+			this.dataGridView3.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellContentClick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 600);
+			this.Controls.Add(this.tabControl1);
+			this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "Form1";
+			this.Text = "Shtuki vsyakie c matritsami i ne s matritsami";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.tabPage2.ResumeLayout(false);
+			this.tabPage2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.DataGridView dataGridView3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label6;
+	}
+}
+
---
+++ 2nd semester/Lab1/Lab1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+	public partial class Form1 : Form
+	{
+		//My functions
+		private double calculate_value(double A, double x)
+		{
+			try
+			{
+				if (x < (4 * A))
+				{
+					return Math.Sqrt(16 * Math.Pow(A, 2) - Math.Pow((x - 4 * A), 2));
+				}
+				else
+				{
+					return (8 * Math.Pow(A, 3)) / (Math.Pow((x - 4 * A), 2) + 4 * Math.Pow(A, 2));
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Failed to calculate value for x = " + Convert.ToString(x));
+				return Double.NaN;
+			}
+		}
+
+		private double calculate_difference(int i, int j, System.Windows.Forms.DataGridView Matrix)
+		{
+			try
+			{
+				return Math.Abs(Convert.ToDouble(Matrix.Rows[i].Cells[j].Value) - Convert.ToDouble(Matrix.Rows[i].Cells[i].Value));
+			}
+			catch
+			{
+				MessageBox.Show("Failed to calculate difference.");
+				return Double.NaN;
+			}
+		}
+
+		//
+
+		public Form1()
+		{
+			InitializeComponent();
+			dataGridView2.RowCount = 4;
+			dataGridView2.ColumnCount = 4;
+			dataGridView3.RowCount = 1;
+			dataGridView3.ColumnCount = 4;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void tabPage1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				int N = Convert.ToInt32(textBox4.Text);
+				double A = Convert.ToDouble(textBox1.Text);
+				double x1 = Convert.ToDouble(textBox2.Text);
+				double dx = Convert.ToDouble(textBox3.Text);
+				dataGridView1.RowCount = N;
+				dataGridView1.ColumnCount = 3;
+				double curr_x = x1;
+				for (int i = 0; i < N; i++)
+				{
+					dataGridView1.Rows[i].Cells[0].Value = i + 1;
+					dataGridView1.Rows[i].Cells[1].Value = "x = " + Convert.ToString(Math.Round(curr_x, 5));
+					dataGridView1.Rows[i].Cells[2].Value = "y = " + Convert.ToString(Math.Round(calculate_value(A, curr_x), 5));
+					curr_x += dx;
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Incorrect input. Try again.");
+			}
+		}
+
+		private void label5_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void label5_Click_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				double difference;
+				for (int i = 0; i < 4; i++)
+				{
+					difference = 0.0;
+					for (int j = 0; j < 4; j++)
+					{
+						difference = Math.Max(difference, calculate_difference(i, j, dataGridView2));
+					}
+					dataGridView3.Rows[0].Cells[i].Value = difference;
+				}
+			}
+			catch
+			{
+				MessageBox.Show("Incorrect input. Try again.");
+			}
+		}
+	}
+}
